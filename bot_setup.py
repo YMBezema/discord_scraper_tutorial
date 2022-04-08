165253a2dbb36b66646a321a3c52f2b443c92800
YMBezema <93187666+YMBezema@users.noreply.github.com>
2022-04-08 10:17:25 +0200

    initial

---
+++ bot_setup.py
@@ -0,0 +1,16 @@
+import discord
+import pandas as pd
+
+client = discord.Client()
+guild = discord.Guild
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    elif message.content.startswith('_'):
+
+        cmd = message.content.split()[0].replace("_","")
+        if len(message.content.split()) > 1:
+            parameters = message.content.split()[1:]
+            

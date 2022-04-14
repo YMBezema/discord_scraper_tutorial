a3ce50ba9e4cf670d0af9b8c82ca7c1d9880a98a
YMBezema <93187666+YMBezema@users.noreply.github.com>
2022-04-14 09:57:54 +0200

    Scraper done

--- bot.py
+++ bot.py
@@ -2,24 +2,26 @@ import os
 import random
 import discord
 from dotenv import load_dotenv
-
 from dadjokes import get_post_reddit
 
-intents = discord.Intents.default()
-intents.members = True
 
 
-#print(random.choice(get_post_reddit('DadJokes')))
-
+# Load environment variables
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
+# Set up bot client with full permissions
+intents = discord.Intents.default()
+intents.members = True
 client = discord.Client(intents=intents)
+
+# Check if bot connection has been made, then write to stdout
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+# Welcome new members to the server through direct message
 @client.event
 async def on_member_join(member):
     await member.create_dm()
@@ -27,25 +29,21 @@ async def on_member_join(member):
         f'Yo {member.name} welkom welkom'
     )
 
+# Runs on every message sent in channels the bot has access to
 @client.event
 async def on_message(message):
+    # If message was sent by the bot, stop
     if message.author == client.user:
         return
-
+    # Respond to command with a random dad joke from the top posts of reddit.com/r/dadjokes
     if message.content == 'dad joke?':
         response = random.choice(get_post_reddit('DadJokes')) 
         await message.channel.send(response)
-    elif message.content == 'raise-exception':
-        raise discord.DiscordException
+    # Otherwise log any message that gets posted in the channel to chat.log
+    else:
+        with open('chat.log', 'a') as f:
+            f.write(f'{message.created_at} - {message.author}:\n{message.content}\n')
 
-@client.event
-async def on_error(event, *args, **kwargs):
-    with open('err.log', 'a') as f:
-        if event == 'on_message':
-            f.write(f'Unhandled message: {args[0]}\n')
-        else:
-            raise
-
-
+# Run client with the bot's token
 client.run(TOKEN)
 
